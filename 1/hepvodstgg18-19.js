48f28709f90bea1766dbb168eb2b4b2811d4a897
zetlnd <53378191+zetlnd@users.noreply.github.com>
2020-03-26 14:03:27 +0300

    3

--- 1/hepvodstgg18-19.js
+++ 1/hepvodstgg18-19.js
@@ -1,1 +1,1 @@
-var player = new Playerjs({id:"player", file:"https://zetlnd.github.io/oZk4HyOc5qv1m1/playerjs/1/pl/hepvodstgg18-19.txt"});
+var player = new Playerjs({id:"player", file:"https://LdieSU7.github.io/1/pl/hepvodstgg18-19.txt"});
